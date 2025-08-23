e2149cda90b08f73de74bc4449da4db67a530260
Neville Brem <contact@nevillebrem.com>
2025-08-23 16:25:33 +0200

    misc and began on home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,24 @@
 <script lang="ts">
+	import Logo from '$lib/components/Logo.svelte';
+	import { ModeWatcher } from 'mode-watcher';
 	import '../app.css';
 	import { authClient } from '$lib/auth-client';
 	import Button, { buttonVariants } from '$lib/components/ui/button/button.svelte';
 	import { getUser } from '$lib/remote/auth.remote';
 	import * as Popover from '$lib/components/ui/popover';
 	import Separator from '$lib/components/ui/separator/separator.svelte';
+	import ModeToggle from '$lib/components/ModeToggle.svelte';
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<link rel="icon" href="/favicon.png" type="image/png" />
+</svelte:head>
+
+<ModeWatcher defaultMode="dark" />
+
 <nav class="flex items-center justify-between p-6">
-	<a href="/" class="text-3xl font-black tracking-tighter">AURI</a>
+	<a href="/" class="flex items-center text-3xl font-black tracking-tighter"><Logo /> AURI</a>
 	<div class="flex items-center gap-2">
 		{#await getUser()}
 			<p>loading user...</p>
@@ -36,6 +45,7 @@
 				>
 			{/if}
 		{/await}
+		<ModeToggle />
 	</div>
 </nav>
 {@render children?.()}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { MessageCircle } from '@lucide/svelte';
+</script>
+
+<main class="rounded-2xl border">
+	<div class="border-b border-border p-6">
+		<h1 class="flex items-center gap-2 text-2xl font-semibold"><MessageCircle /> Document Chat</h1>
+	</div>
+</main>
