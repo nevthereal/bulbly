df0ba6d1a1e748fb7f353bf15d14d5eb6c9f4fce
Neville Brem <contact@nevillebrem.com>
2025-10-06 16:46:40 +0200

    worked on the file viewer

--- src/lib/components/KnowledgeBase.svelte
+++ src/lib/components/KnowledgeBase.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-	import { Trash2, Upload, Workflow } from '@lucide/svelte';
+	import { FileText, Trash2, Upload, Workflow } from '@lucide/svelte';
 
 	import Input from './ui/input/input.svelte';
-	import Button from './ui/button/button.svelte';
+	import Button, { buttonVariants } from './ui/button/button.svelte';
 	import { Progress } from '$lib/components/ui/progress/index.js';
+	import * as Dialog from '$lib/components/ui/dialog';
 
 	import { generateSvelteHelpers } from '@uploadthing/svelte';
 	import type { MyRouter } from '$lib/server/uploadthing';
@@ -12,8 +13,10 @@
 
 	let filesToBeUploaded: FileList | undefined = $state(undefined);
 	let uploadProgress: number | null = $state(null);
+	let open = $state(false);
 
 	let { projectId }: { projectId: string } = $props();
+	const { files: kbFiles } = await getProject(projectId);
 
 	const { createUploadThing } = generateSvelteHelpers<MyRouter>({
 		url: `/project/${projectId}/upload`
@@ -24,49 +27,76 @@
 			uploadProgress = progress;
 		},
 		uploadProgressGranularity: 'all',
-		onUploadBegin: (file) => {
-			toast(`Starting upload of ${file}...`);
-		},
 		onClientUploadComplete: () => {
 			filesToBeUploaded = undefined;
 			uploadProgress = null;
+			toast.success('Upload Completed');
+			open = false;
+
+			getProject(projectId).refresh();
 		}
 	});
-
-	const { files: kbFiles } = await getProject(projectId);
 </script>
 
-<div class="h-full w-lg rounded-2xl border p-6">
+<div class="flex h-full w-lg flex-col rounded-2xl border p-6">
 	<h1 class="flex items-center gap-2 border-b pb-2 text-2xl font-semibold">
 		<Workflow /> Knowledge Base
 	</h1>
-	<Input type="file" multiple bind:files={filesToBeUploaded} />
-	{#if filesToBeUploaded}
-		{@const arrayedFiles = Array.from(filesToBeUploaded)}
-		<ul>
-			{#each arrayedFiles as f (f)}
-				<li class="my-1 flex items-center justify-between">
-					{f.name}
-					<Button
-						size="icon"
-						variant="destructive"
-						onclick={() => {
-							const dt = new DataTransfer();
-							const filteredFiles = arrayedFiles.filter((filter) => filter.name != f.name);
 
-							filteredFiles.forEach((i) => dt.items.add(i));
-
-							filesToBeUploaded = dt.files;
-						}}><Trash2 /></Button
-					>
+	<h2 class="py-2 font-medium">Your files:</h2>
+	<div class="mb-2 flex-1 overflow-y-auto">
+		<ul class="grid grid-cols-2 gap-2">
+			{#each kbFiles as file (file.id)}
+				<li class="flex flex-col justify-between gap-2 rounded-md border p-2">
+					{#if file.type === 'image/jpeg' || file.type === 'image/png'}
+						<img src={file.utURL} alt="" class="rounded-sm" />
+					{:else if file.type === 'application/pdf'}
+						<div class="flex h-full items-center justify-center rounded-sm bg-muted">
+							<FileText class="size-20 text-muted-foreground" />
+						</div>
+					{/if}
+					{file.name}
 				</li>
 			{/each}
 		</ul>
-		<Button disabled={$isUploading} onclick={() => startUpload(arrayedFiles)}
-			><Upload /> Upload</Button
-		>
-		{#if uploadProgress}
-			<Progress value={uploadProgress} />
-		{/if}
-	{/if}
+	</div>
+
+	<Dialog.Root bind:open>
+		<Dialog.Trigger class={buttonVariants()}>Add files to Knowledge Base</Dialog.Trigger>
+		<Dialog.Content>
+			<Dialog.Header>
+				<Dialog.Title>Upload files</Dialog.Title>
+			</Dialog.Header>
+			<Input type="file" multiple bind:files={filesToBeUploaded} />
+			{#if filesToBeUploaded}
+				{@const arrayedFiles = Array.from(filesToBeUploaded)}
+				<ul>
+					{#each arrayedFiles as f (f)}
+						<li class="my-1 flex items-center justify-between">
+							{f.name}
+							<Button
+								size="icon"
+								variant="destructive"
+								onclick={() => {
+									const dt = new DataTransfer();
+									const filteredFiles = arrayedFiles.filter((filter) => filter.name != f.name);
+
+									filteredFiles.forEach((i) => dt.items.add(i));
+
+									filesToBeUploaded = dt.files;
+								}}><Trash2 /></Button
+							>
+						</li>
+					{/each}
+				</ul>
+				<Button
+					disabled={$isUploading || Array.from(filesToBeUploaded).length === 0}
+					onclick={() => startUpload(arrayedFiles)}><Upload /> Upload</Button
+				>
+				{#if uploadProgress}
+					<Progress value={uploadProgress} />
+				{/if}
+			{/if}
+		</Dialog.Content>
+	</Dialog.Root>
 </div>
