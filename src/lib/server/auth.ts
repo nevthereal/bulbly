653f831f144afeffdabc774ec4511585c13f52e2
Neville Brem <contact@nevillebrem.com>
2025-10-10 13:26:21 +0200

    add secret

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -2,7 +2,7 @@ import { betterAuth } from 'better-auth';
 import * as schema from './db/schema';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { db } from './db';
-import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
+import { BETTER_AUTH_SECRET, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
 
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
@@ -14,7 +14,8 @@ export const auth = betterAuth({
 			clientId: GOOGLE_CLIENT_ID,
 			clientSecret: GOOGLE_CLIENT_SECRET
 		}
-	}
+	},
+	secret: BETTER_AUTH_SECRET
 });
 
 export type Auth = typeof auth;
