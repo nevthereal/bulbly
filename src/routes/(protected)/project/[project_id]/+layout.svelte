5c1a75170e02ed800b59634fe68a4aa07981f894
Neville Brem <contact@nevillebrem.com>
2025-10-25 16:59:19 +0200

    add skeleton & fix issue with alert dialog

--- src/routes/(protected)/project/[project_id]/+layout.svelte
+++ src/routes/(protected)/project/[project_id]/+layout.svelte
@@ -8,46 +8,55 @@
 	import { buttonVariants } from '$lib/components/ui/button/button.svelte';
 	import { goto } from '$app/navigation';
 	import { toast } from 'svelte-sonner';
+	import { Skeleton } from '$lib/components/ui/skeleton/index.js';
 
 	let { params, children } = $props();
 </script>
 
 <main class="flex h-[90dvh] flex-col p-2">
 	<!-- TODO -->
-	<h1 class="mb-4 ml-4 text-3xl font-bold">prj totle</h1>
-	<div class="flex items-center gap-2">
+	<div class="mx-4 mb-2 flex items-center justify-between text-3xl">
 		<svelte:boundary>
-			<AlertDialog.Root>
-				<AlertDialog.Trigger class={buttonVariants({ size: 'sm', variant: 'destructive' })}
-					><Trash2 /> Delete</AlertDialog.Trigger
-				>
-				<AlertDialog.Content>
-					<AlertDialog.Header>
-						<AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
-						<AlertDialog.Description>
-							This action cannot be undone. This will permanently delete this project, all of it's
-							files, threads and tool results.
-						</AlertDialog.Description>
-					</AlertDialog.Header>
-					<AlertDialog.Footer>
-						<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
-						<AlertDialog.Action
-							onclick={() =>
-								toast.promise(
-									deleteProject(params.project_id).then(() => goto('/')),
-									{
-										loading: 'Deleting project…',
-										success: 'Deletion sucessful',
-										error: 'An error occured during deletion'
-									}
-								)}
-							class={buttonVariants({ variant: 'destructive' })}>Continue</AlertDialog.Action
-						>
-					</AlertDialog.Footer>
-				</AlertDialog.Content>
-			</AlertDialog.Root>
+			{#snippet pending()}
+				<Skeleton class="h-lh w-48 rounded-full" />
+			{/snippet}
+			<h1 class="text-3xl font-bold">
+				{(await getProject()).name}
+			</h1>
+			<div class="flex items-center gap-2">
+				<AlertDialog.Root>
+					<AlertDialog.Trigger class={buttonVariants({ size: 'sm', variant: 'destructive' })}
+						><Trash2 /> Delete</AlertDialog.Trigger
+					>
+					<AlertDialog.Content>
+						<AlertDialog.Header>
+							<AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
+							<AlertDialog.Description>
+								This action cannot be undone. This will permanently delete this project, all of it's
+								files, threads and tool results.
+							</AlertDialog.Description>
+						</AlertDialog.Header>
+						<AlertDialog.Footer>
+							<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+							<AlertDialog.Action
+								onclick={() =>
+									toast.promise(
+										deleteProject(params.project_id).then(() => goto('/')),
+										{
+											loading: 'Deleting project…',
+											success: 'Deletion sucessful',
+											error: 'An error occured during deletion'
+										}
+									)}
+								class={buttonVariants({ variant: 'destructive' })}>Continue</AlertDialog.Action
+							>
+						</AlertDialog.Footer>
+					</AlertDialog.Content>
+				</AlertDialog.Root>
+			</div>
 		</svelte:boundary>
 	</div>
+
 	<div class="flex items-center justify-between"></div>
 	<div class="flex h-full gap-4 overflow-scroll">
 		<KnowledgeBase projectId={params.project_id} />
